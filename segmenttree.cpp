98fc0a68d9b8dde0d212cc386bab632372ede652
Jakub Kuczkowiak <jakubkuczkowiak@gmail.com>
2018-08-26 04:15:29 +0200

    SegmentTree: packed in template class

--- segmenttree.cpp
+++ segmenttree.cpp
@@ -1,55 +1,74 @@
 /* https://codeforces.com/blog/entry/18051 */
 
 /*
-Segment Tree efficient implementation
+SegmentTree efficient implementation.
+SegmentTree(int n): initializes tree with given size.
+Remember to initialize all leafs using initialize_leaf function and when finished call build.
+First leaf is at position t[n]
 */
-
-const int N = 1e5;  // limit for array size
-int n;  // array size
-int t[2 * N];
-
-void build() {  // build the tree
-	for (int i = n - 1; i > 0; --i) t[i] = t[i << 1] + t[i << 1 | 1];
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////
-void modify_point(int p, int value) {  // set value at position p
-	for (t[p += n] = value; p > 1; p >>= 1) {
-		t[p >> 1] = t[p] + t[p ^ 1]; // t[p ^ 1] is the other child
+template <class T>
+class SegmentTree
+{
+public:
+	SegmentTree(int n) {
+		this->n = n;
+		t = new T[2 * n];
 	}
-}
 
-int query_interval(int l, int r) {  // sum on interval [l, r)
-	int res = 0;
-	for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-		if (l & 1) res += t[l++];
-		if (r & 1) res += t[--r];
+	~SegmentTree() {
+		delete[] t;
 	}
-	return res;
-}
-////////////////////////////////////////////////////////////////////////////////////////////
 
-////////////////////////////////////////////////////////////////////////////////////////////
-void modify_interval(int l, int r, int value) {
-	for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
-		if (l & 1) t[l++] += value;
-		if (r & 1) t[--r] += value;
+	__inline void initialize_leaf(int index, T value) {
+		t[n + index] = value;
 	}
-}
 
-int query_point(int p) {
-	int res = 0;
-	for (p += n; p > 0; p >>= 1) res += t[p];
-	return res;
-}
-////////////////////////////////////////////////////////////////////////////////////////////
+	// We use build to reduce complexity to O(n) instead of O(nlog n)
+	void build() {
+		for (int i = n - 1; i > 0; --i) t[i] = t[i << 1] + t[i << 1 | 1];
+	}
 
-int main() {
-	scanf("%d", &n);
-	for (int i = 0; i < n; ++i) scanf("%d", t + n + i);
-	build();
-	modify(0, 1);
-	printf("%d\n", query(3, 11));
-	return 0;
-}
+	void modify_point(int p, T value) {  // set value at position p?
+		for (t[p += n] = value; p > 1; p >>= 1) {
+			t[p >> 1] = t[p] + t[p ^ 1]; // t[p ^ 1] is the other child
+		}
+	}
 
+	T query_interval(int l, int r) {  // sum on interval [l, r)?
+		T res = 0;
+		for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+			if (l & 1) res += t[l++];
+			if (r & 1) res += t[--r];
+		}
+		return res;
+	}
+
+	void modify_interval(int l, int r, T value) { // modify interval [l, r)?
+		for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+			if (l & 1) t[l++] += value;
+			if (r & 1) t[--r] += value;
+		}
+	}
+
+	T query_point(int p) {
+		T res = 0;
+		for (p += n; p > 0; p >>= 1) res += t[p];
+		return res;
+	}
+
+	// If at some point after modifications we need to inspect all the elements in the array, 
+	// we can push all the modifications to the leaves using the following code. 
+	// After that we can just traverse elements starting with index n.
+	// Time complexity: O(n)
+	void push() {
+		for (int i = 1; i < n; ++i) {
+			t[i << 1] += t[i];
+			t[i << 1 | 1] += t[i];
+			t[i] = 0;
+		}
+	}
+
+private:
+	int* t;
+	int n;
+};
