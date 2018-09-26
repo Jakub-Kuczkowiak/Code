cf0e16e9e04e42490769d12f046feba0e7c36fb3
Jakub Kuczkowiak <jakubkuczkowiak@gmail.com>
2018-09-27 00:29:43 +0200

    hash, binary_answer variant, compare

--- contest-template.cpp
+++ contest-template.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <set>
+#include <unordered_set>
 #include <map>
 #include <unordered_map>
 #include <algorithm>
--- useful.cpp
+++ useful.cpp
@@ -1,10 +1,19 @@
 /* HASHING */
+// Probably should be something different than xor
 
 struct pairhash { 
 public:
 	template <typename T, typename U>
 	std::size_t operator()(const std::pair<T, U> &x) const {
 		return std::hash<T>()(x.first) ^ std::hash<U>()(x.second);
+	}
+};
+
+struct tuplehash {
+public:
+	template <typename T, typename U, typename W>
+	std::size_t operator()(const std::tuple<T, U, W> &x) const {
+		return std::hash<T>()(get<0>(x)) ^ std::hash<U>()(get<1>(x)) ^ std::hash<W>()(get<2>(x));
 	}
 };
 
@@ -28,7 +37,7 @@ bool (*solve)(long long) -> bool (*solve)(long long, ...)
 in case you need to pass more arguments easily
 */
 
-// case: no, no, no, yes, yes, yes
+// case: no, no, no, yes, yes, yes; finds the first 'yes'
 long long binary_answer(long long l, long long r, bool (*solve)(long long)) {
 	if (l > r) return -1; // base case; no found element
 
@@ -48,6 +57,26 @@ long long binary_answer(long long l, long long r, bool (*solve)(long long)) {
 	}
 }
 
+// case: yes, yes, yes, no, no, no; finds the last 'yes'
+long long binary_answer2(long long l, long long r, bool(*solve)(long long)) {
+	if (l > r) return -1; // base case; no found element
+
+	long long m = l + (r - l) / 2;
+
+	if (solve(m)) { // we met some 'yes' here
+		long long answer = binary_answer2(m + 1, r, solve);
+		if (answer != -1) {
+			return answer;
+		}
+		else {
+			return m;
+		}
+	}
+	else {
+		return binary_answer2(l, m - 1, solve);
+	}
+}
+
 // binary search for double answer 
 double binary_answer(double low, double high, int iterations) {
 	for (int i = 0; i < iterations; i++) {
@@ -57,3 +86,21 @@ double binary_answer(double low, double high, int iterations) {
 
 	return high;
 }
+
+/* ROUNDING */
+// If you ever need to combine a lot of multiplications and divisions in the future, where intermediate values might get very small, it helps to work in log space:
+A * B / C = exp( log(A) + log(B) - log(C) )
+// it avoids rounding all the time
+
+
+/* COMPARATOR */
+// Difference between declaration and object created
+class Compare {
+public:
+    bool operator() (T a , T b) {
+        return true;
+    }
+}compare;
+
+priority_queue<T, vector<T>, Compare> pq;
+sort(v.begin(), v.end(), compare);
